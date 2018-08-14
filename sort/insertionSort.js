18bd645c461e41366b7145e636981335051a3ce5
Chloe Chong <chloechong.us@gmail.com>
2018-08-14 11:25:18 -0400

    Fixed error in insertion sort

--- sort/insertionSort.js
+++ sort/insertionSort.js
@@ -1,6 +1,6 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    let j = 1;
+    let j = i;
     while (j > 0 && arr[j] < arr[j - 1]) {
       [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
       j--;
