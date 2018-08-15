6f30b5d4ef8a49201a7a6fc017bb432f5cb5cf8a
Chloe Chong <chloechong.us@gmail.com>
2018-08-15 12:56:13 -0400

    Added binary search solution to LIS

--- optimisation/longestIncreasingSubsequence.js
+++ optimisation/longestIncreasingSubsequence.js
@@ -1,3 +1,33 @@
+// function longestIncreasingSubsequence(sequence, idx = 0, base = -Infinity) {
+//   if (idx === sequence.length) return 0;
+//   const num = sequence[idx];
+//   const whenExcluded = longestIncreasingSubsequence(sequence, idx + 1, base);
+//   if (num <= base) return whenExcluded;
+//   const whenIncluded = 1 + longestIncreasingSubsequence(sequence, idx + 1, num);
+//   console.log(Math.max(whenIncluded, whenExcluded));
+//   return Math.max(whenIncluded, whenExcluded);
+// }
+
+// O(n*logm) using binary search
+// function longestIncreasingSubsequence(nums) {
+//   if (!nums.length) return 0;
+//   const dp = [nums[0]];
+//   for (let i = 1; i < nums.length; i++) {
+//     let min = 0,
+//       max = nums.length;
+//     while (min < max) {
+//       const mid = Math.floor((max + min) / 2);
+//       // console.log('mid', mid);
+//       // console.log(dp[mid]);
+//       dp[mid] < nums[i] ? (min = mid + 1) : (max = mid);
+//       console.log('while', dp);
+//     }
+//     dp[max] = nums[i];
+//     console.log('for', dp);
+//   }
+//   return dp.length;
+// }
+
 function longestIncreasingSubsequence(arr) {
   const res = new Array(arr.length).fill(1);
   for (let i = 1; i < arr.length; i++) {
@@ -29,5 +59,6 @@ const test = longestIncreasingSubsequence([
   27,
   28,
 ]);
+const test2 = longestIncreasingSubsequence([3, 4, 2, 1, 10, 6]);
 
-console.log(test);
+console.log(test, test2);
