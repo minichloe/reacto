29b3cc0b70bd55a633eb7fbd7b443aea328f3cde
Chloe Chong <chloechong.us@gmail.com>
2018-08-16 16:02:57 -0400

    Added circular queue solution

--- queues/circularQueue.js
+++ queues/circularQueue.js
@@ -1,7 +1,39 @@
 function circularQueue(size) {
-  this.queue = new Array(size);
+  this.queue = new Array(size).fill(null);
   this.size = size;
-  this.front = 0;
+  this.front = this.queue[0];
   this.end = 0;
 }
-circularQueue.enQueue = function(val) {};
+circularQueue.enQueue = function(val) {
+  if (this.end === this.size) return false;
+  if (this.front === null) this.queue[0] = val;
+  else this.queue[this.end] = val;
+  this.end++;
+  return true;
+};
+
+circularQueue.deQueue = function() {
+  if (this.end === null) return false;
+  const idx = this.end === this.size ? this.end - 1 : this.end;
+  this.queue[idx] = null;
+  this.end--;
+  return true;
+};
+
+circularQueue.Front = function() {
+  return this.front || -1;
+};
+
+circularQueue.Rear = function() {
+  return this.end === this.size
+    ? this.queue[this.end - 1]
+    : this.queue[this.end];
+};
+
+circularQueue.isEmpty = function() {
+  return this.front === null;
+};
+
+circularQueue.isFull = function() {
+  return this.end === this.size;
+};
