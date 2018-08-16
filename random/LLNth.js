990c38f70be1f552ac63246bc7b1911d66551abc
Chloe Chong <chloechong.us@gmail.com>
2018-08-16 16:26:19 -0400

    traverse nth node from last value in linked list

---
+++ random/LLNth.js
@@ -0,0 +1,16 @@
+// Given a linked list and a number (num < LL length)
+// Return the nth from the last value in the link list
+// Eg. LL: 11 -> 22 -> 33 -> 44 -> 55, num: 3; return 33;
+
+function traverseNode(startNode, num) {
+  let fastNode = startNode;
+  while (num >= 0) {
+    fastNode = fastNode.next;
+    num--;
+  }
+  while (fastNode) {
+    startNode = startNode.next;
+    fastNode = fastNode.next;
+  }
+  return startNode;
+}
