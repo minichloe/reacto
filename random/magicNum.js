3ffc209759c43d75eb42a42840295806d38160b7
Chloe Chong <chloechong.us@gmail.com>
2018-08-16 14:29:26 -0400

    Magic num count version

--- random/magicNum.js
+++ random/magicNum.js
@@ -1,3 +1,4 @@
+// Return true if adding + or - with all numbers in the array will equal the target
 function magicNum(target, arr) {
   const allNums = new Set([0]);
   for (let i = 0; i < arr.length; i++) {
@@ -10,9 +11,13 @@ function magicNum(target, arr) {
       allNums.add(minusNum);
     }
   }
-  console.log(allNums);
   return allNums.has(target);
 }
 
-const test = magicNum(11, [1, 2, 3, 4]);
+// Return num of different ways numbers can compute to target
+function magicNumCount(target, arr) {
+  const
+}
+
+const test = magicNum(11, [1, 2, 3, 4, 5]);
 console.log(test);
