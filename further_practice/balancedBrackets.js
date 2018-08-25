3a7443fec43de693c912f5da818c26af56bbec80
Chloe Chong <chloechong.us@gmail.com>
2018-08-25 22:56:23 +0200

    Added tests and console logs

--- further_practice/balancedBrackets.js
+++ further_practice/balancedBrackets.js
@@ -1,8 +1,17 @@
 function balancedBrackets(str) {
-  const brackets = /[[\](){}]/g;
+  const bracketsRegex = /[[\](){}]/g;
+  const brackets = str.match(bracketsRegex);
+  if (!str.length || !brackets.length) return true;
+  if (brackets.length % 2) return false;
   const pairs = {
     '[': ']',
     '(': ')',
     '{': '}',
   };
 }
+
+// balancedBrackets('({)}'); // false
+// balancedBrackets('({[]})'); // true
+const test = balancedBrackets('[][(){}'); // false
+const test2 = balancedBrackets('text ( is allowed ){rwwrwrrww [] ()}'); // true
+console.log(test, test2);
