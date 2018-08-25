f3cf8301cfdc470cff1b2761fa0eb1ddbf925281
Chloe Chong <chloechong.us@gmail.com>
2018-08-25 23:13:49 +0200

    Finished bracket pairs

--- further_practice/balancedBrackets.js
+++ further_practice/balancedBrackets.js
@@ -8,10 +8,19 @@ function balancedBrackets(str) {
     '(': ')',
     '{': '}',
   };
+  const bracketStack = [];
+  for (let i = 0; i < brackets.length; i++) {
+    if (brackets[i] in pairs) bracketStack.push(brackets[i]);
+    else if (pairs[bracketStack[bracketStack.length - 1]] === brackets[i])
+      bracketStack.pop();
+    else return false;
+  }
+  return true;
 }
 
-// balancedBrackets('({)}'); // false
-// balancedBrackets('({[]})'); // true
+const one = balancedBrackets('({)}'); // false
+const two = balancedBrackets('({[]})'); // true
 const test = balancedBrackets('[][(){}'); // false
 const test2 = balancedBrackets('text ( is allowed ){rwwrwrrww [] ()}'); // true
+console.log(one, two);
 console.log(test, test2);
