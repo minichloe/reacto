6b1df6696298bea3c175f119fb55697cd56672ab
Chloe Chong <chloechong.us@gmail.com>
2018-08-29 09:09:57 +0200

    solved palindrome number

--- numbers/palindromeNumber.js
+++ numbers/palindromeNumber.js
@@ -1,5 +1,17 @@
 function isPalindrome(num) {
-  console.log(num % 10);
+  if (num < 0) return false;
+  const nums = [];
+  while (num > 0) {
+    nums.push(num % 10);
+    num = Math.floor(num / 10);
+  }
+  for (let i = 0, j = nums.length - 1; i < j; ) {
+    if (nums[i] !== nums[j]) return false;
+    i++;
+    j--;
+  }
+  return true;
 }
 
-isPalindrome(121);
+const test = isPalindrome(10013);
+console.log(test);
