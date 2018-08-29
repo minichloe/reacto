653f2a732a63b412cea3db2d9e32902baf54b6e6
Chloe Chong <chloechong.us@gmail.com>
2018-08-29 09:15:14 +0200

    Optimised solution for palindrome number

--- numbers/palindromeNumber.js
+++ numbers/palindromeNumber.js
@@ -1,17 +1,28 @@
+// function isPalindrome(num) {
+//   if (num < 0) return false;
+//   const nums = [];
+//   while (num > 0) {
+//     nums.push(num % 10);
+//     num = Math.floor(num / 10);
+//   }
+//   for (let i = 0, j = nums.length - 1; i < j; ) {
+//     if (nums[i] !== nums[j]) return false;
+//     i++;
+//     j--;
+//   }
+//   return true;
+// }
+
 function isPalindrome(num) {
   if (num < 0) return false;
-  const nums = [];
+  let reverse = 0,
+    initial = num;
   while (num > 0) {
-    nums.push(num % 10);
+    reverse = reverse * 10 + (num % 10);
     num = Math.floor(num / 10);
   }
-  for (let i = 0, j = nums.length - 1; i < j; ) {
-    if (nums[i] !== nums[j]) return false;
-    i++;
-    j--;
-  }
-  return true;
+  return initial === reverse;
 }
 
-const test = isPalindrome(10013);
+const test = isPalindrome(102301);
 console.log(test);
