681235d30ae18eb0568132a2c289b59b8a90d58d
Chloe Chong <chloechong.us@gmail.com>
2018-08-30 18:19:37 +0200

    add two linked lists together and return new linked list

---
+++ data-structures/addTwoNumbersLL.js
@@ -0,0 +1,26 @@
+function addTwoNumbers(l1, l2) {
+  let p1 = l1,
+    p2 = l2;
+  const s1 = [],
+    s2 = [];
+  while (p1) {
+    s1.push(p1.val);
+    p1 = p1.next;
+  }
+  while (p2) {
+    s2.push(p2.val);
+    p2 = p2.next;
+  }
+  let sum = 0;
+  let list = new ListNode(0);
+  while (s1.length || s2.length) {
+    if (s1.length) sum += s1.pop();
+    if (s2.length) sum += s2.pop();
+    list.val = sum % 10;
+    sum = Math.floor(sum / 10);
+    let head = new ListNode(sum);
+    head.next = list;
+    list = head;
+  }
+  return list.val === 0 ? list.next : list;
+}
