0629a9467d5621935445c092e652785d660d2b12
Chloe Chong <chloechong.us@gmail.com>
2018-08-30 17:38:24 +0200

    binary search for range

---
+++ numbers/searchPosition.js
@@ -0,0 +1,22 @@
+function searchRange(nums, target) {
+  const res = [-1, -1];
+  if (nums[0] > target || nums[nums.length - 1] < target) return res;
+  let mid = Math.floor(nums.length / 2);
+  let left = 0,
+    right = nums.length - 1;
+  for (left, right; left < right; ) {
+    nums[mid] > target ? (right = mid - 1) : (left = mid + 1);
+    if (nums[mid] === target) {
+      (left = mid - 1), (right = mid + 1);
+      while (nums[left] === target || nums[right] === target) {
+        if (nums[left] === target) left--;
+        if (nums[right] === target) right++;
+      }
+      return [left + 1, right - 1];
+    }
+  }
+  return res;
+}
+
+const test = searchRange([5, 7, 7, 8, 8, 10], 8);
+console.log(test);
