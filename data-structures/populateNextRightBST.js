73b6ada429d6751ec0465ba025e501433b78cfea
Chloe Chong <chloechong.us@gmail.com>
2018-09-05 13:38:33 -0400

    Added breadth first search answer to populate next right BST

--- data-structures/populateNextRightBST.js
+++ data-structures/populateNextRightBST.js
@@ -10,3 +10,19 @@ function connect(root, next = null) {
     root.next ? root.next.left : null
   );
 }
+
+// Solution by breadth first search then connecting
+function connectWithBFS(root) {
+  if (!root) return;
+  const queue = [root];
+  while (queue.length) {
+    const size = queue.length;
+    const copy = queue.slice();
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      node.next = copy[i + 1] || null;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+}
