45e07cbb7596a7c32cbdbbddbbe89b6b8091ba85
Chloe Chong <chloechong.us@gmail.com>
2018-09-06 14:38:00 -0400

    Added recursive solution for LL

--- data-structures/reverseLL.js
+++ data-structures/reverseLL.js
@@ -1,11 +1,20 @@
-const reverseList = head => {
-  if (!head) return head;
-  let dummy = new ListNode(head.val);
-  while (head.next) {
-    head = head.next;
-    const node = new ListNode(head.val);
-    node.next = dummy;
-    dummy = node;
-  }
-  return dummy;
-};
+// const reverseList = head => {
+//   if (!head) return head;
+//   let dummy = new ListNode(head.val);
+//   while (head.next) {
+//     head = head.next;
+//     const node = new ListNode(head.val);
+//     node.next = dummy;
+//     dummy = node;
+//   }
+//   return dummy;
+// };
+
+// Recursive solution
+// const reverseList = (head, dummy = null) => {
+//   if (!head) return dummy;
+//   const node = new ListNode(head.val);
+//   node.next = dummy;
+//   dummy = node;
+//   return reverseList(head.next, dummy);
+// };
