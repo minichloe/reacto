eedc23a09b4345bb0ce34a5c409d6f1f1ffac13e
Chloe Chong <chloechong.us@gmail.com>
2018-09-06 14:06:26 -0400

    Added console logs for lru cache

--- further_practice/LRUCache.js
+++ further_practice/LRUCache.js
@@ -20,4 +20,12 @@ class LRUCache {
   }
 }
 
-const cache = new LRUCache(2);
+const cache = new LRUCache(4);
+cache.put(1, 1);
+cache.put(2, 2);
+// console.log(cache.get(1));
+// console.log(cache.get(3));
+cache.put(3, 3);
+cache.put(4, 4);
+cache.get(3);
+console.log(cache);
