c7efe81f624a9b80eee50dd78e820287f2c956fc
Chloe Chong <chloechong.us@gmail.com>
2018-09-06 14:30:59 -0400

    iterative solution one pass for reverse LL

--- data-structures/reverseLL.js
+++ data-structures/reverseLL.js
@@ -1,1 +1,12 @@
-const reverseList = head => {};
+const reverseList = head => {
+  if (!head) return head;
+  let list = head;
+  let dummy = new ListNode(list.val);
+  while (list.next) {
+    list = list.next;
+    const node = new ListNode(list.val);
+    node.next = dummy;
+    dummy = node;
+  }
+  return dummy;
+};
