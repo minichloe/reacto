9da3f714c7a866111bd9b506c611acf73a8d1565
Chloe Chong <chloechong.us@gmail.com>
2018-09-06 14:02:35 -0400

    LRU cache with map

--- further_practice/LRUCache.js
+++ further_practice/LRUCache.js
@@ -4,7 +4,20 @@ class LRUCache {
     this.map = new Map();
   }
   get(key) {
-    if (this.map.get(key) !== undefined) return this.map.get(key);
-    else return -1;
+    if (this.map.has(key)) {
+      const val = this.map.get(key);
+      this.map.delete(key);
+      this.map.set(key, val);
+      return val;
+    } else return -1;
+  }
+  put(key, val) {
+    this.map.set(key, val);
+    let keys = this.map.keys();
+    while (this.map.size > this.capacity) {
+      this.map.delete(keys.next().value);
+    }
   }
 }
+
+const cache = new LRUCache(2);
