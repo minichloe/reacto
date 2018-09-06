90a4598f572f341a2c5579e81ca9d65f834637da
Chloe Chong <chloechong.us@gmail.com>
2018-09-06 09:37:06 -0400

    Added console logs and tests to rain water

--- further_practice/rainWater.js
+++ further_practice/rainWater.js
@@ -14,3 +14,18 @@ function trap(blocks) {
   }
   return water;
 }
+
+const a = [0, 0, 1, 2, 4, 3, 2, 5, 0, 0, 2, 1];
+console.log(a); // 7
+
+const b = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+console.log(b); // 6
+
+const c = [0, 3, 0, 1, 0, 0, 0, 1, 0, 2];
+console.log(c); // 12
+
+const d = [0, 1, 0, 3, 5, 0, 0, 0, 2, 0, 1];
+console.log(d); // 8
+
+const e = [0, 5, 3, 2, 8, 8, 1, 1, 2, 4, 3, 3, 7, 1, 2, 4, 3, 2];
+console.log(e); // 38
