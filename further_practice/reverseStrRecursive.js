691d6089917cce335523ebb0960211ddb3acc286
Chloe Chong <chloechong.us@gmail.com>
2018-09-06 10:21:49 -0400

    Added reverse str optimal

--- further_practice/reverseStrRecursive.js
+++ further_practice/reverseStrRecursive.js
@@ -1,17 +1,22 @@
+// function reverseString(str) {
+//   function reverseStringHelper(acc, rest) {
+//     if (rest.length === 0) {
+//       return acc;
+//     } else {
+//       const head = rest.charAt(rest.length - 1);
+//       const tail = rest.substring(rest, rest.length - 1);
+
+//       return reverseStringHelper(acc + head, tail);
+//     }
+//   }
+
+//   return reverseStringHelper('', str);
+// }
+
 function reverseString(str) {
-  function reverseStringHelper(acc, rest) {
-    if (rest.length === 0) {
-      return acc;
-    } else {
-      const head = rest.charAt(rest.length - 1);
-      const tail = rest.substring(rest, rest.length - 1);
-
-      return reverseStringHelper(acc + head, tail);
-    }
-  }
-
-  return reverseStringHelper('', str);
+  if (str.length === 1) return str;
+  return str.charAt(str.length - 1) + reverseString(str.slice(0, -1));
 }
 
-const test = reverseString('Fullstack');
+const test = reverseString('testing');
 console.log(test);
