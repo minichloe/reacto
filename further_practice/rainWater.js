fe72fd0a9e5518e28a556c9cba600113adee5e7e
Chloe Chong <chloechong.us@gmail.com>
2018-09-06 09:35:50 -0400

    Double loop for rain water

--- further_practice/rainWater.js
+++ further_practice/rainWater.js
@@ -1,5 +1,16 @@
 function trap(blocks) {
-  const rightMax = [];
-  let rightBlock = 0;
-  for (let i = blocks.length - 1; i >= 0; i--) {}
+  const rightBlocks = [];
+  let rightMax = 0;
+  for (let i = blocks.length - 1; i >= 0; i--) {
+    rightMax = Math.max(rightMax, blocks[i]);
+    rightBlocks[i] = rightMax;
+  }
+  let water = 0;
+  let leftMax = 0;
+  for (let i = 0; i < blocks.length; i++) {
+    leftMax = Math.max(leftMax, blocks[i]);
+    rightMax = rightBlocks[i];
+    water += Math.min(leftMax, rightMax) - blocks[i];
+  }
+  return water;
 }
