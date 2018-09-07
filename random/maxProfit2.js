aeaa78f8a52b88d5af9ab95e754635112b88b500
Chloe Chong <chloechong.us@gmail.com>
2018-09-07 10:00:47 -0400

    Optimized max profit solution

--- random/maxProfit2.js
+++ random/maxProfit2.js
@@ -2,7 +2,7 @@ function maxProfit(prices) {
   if (prices.length < 2) return 0;
   let profit = 0,
     curr = prices[0];
-  for (let i = 0; i < prices.length; i++) {
+  for (let i = 1; i < prices.length; i++) {
     if (prices[i] > curr) profit += prices[i] - curr;
     curr = prices[i];
   }
