505696d714f2118a811c85c629e09cbaf9d15fcd
Chloe Chong <chloechong.us@gmail.com>
2018-09-08 10:31:44 -0400

    O(n) solution for max sub array

--- random/maxSubArray.js
+++ random/maxSubArray.js
@@ -1,1 +1,12 @@
-function maxSubArray(nums) {}
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+function maxSubArray(nums) {
+  let curr = nums[0];
+  const sums = [curr];
+  for (let i = 1; i < nums.length; i++) {
+    const sum = nums[i] + curr;
+    if (sum <= nums[i]) curr = nums[i];
+    else curr = sum;
+    sums.push(curr);
+  }
+  return Math.max(...sums);
+}
