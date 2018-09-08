7935264e7cfd36c2085e3cd2eb27a33674a423f2
Chloe Chong <chloechong.us@gmail.com>
2018-09-08 10:36:56 -0400

    Added optimized solution for max sub array

--- random/maxSubArray.js
+++ random/maxSubArray.js
@@ -1,14 +1,25 @@
 // Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+// function maxSubArray(nums) {
+//   let curr = nums[0];
+//   const sums = [curr];
+//   for (let i = 1; i < nums.length; i++) {
+//     const sum = nums[i] + curr;
+//     if (sum <= nums[i]) curr = nums[i];
+//     else curr = sum;
+//     sums.push(curr);
+//   }
+//   return Math.max(...sums);
+// }
+
+// Updated version for O(n) time and O(1) space
 function maxSubArray(nums) {
   let curr = nums[0];
-  const sums = [curr];
+  let max = curr;
   for (let i = 1; i < nums.length; i++) {
-    const sum = nums[i] + curr;
-    if (sum <= nums[i]) curr = nums[i];
-    else curr = sum;
-    sums.push(curr);
+    curr = Math.max(nums[i] + curr, nums[i]);
+    max = Math.max(max, curr);
   }
-  return Math.max(...sums);
+  return max;
 }
 
 const test = maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
