758e8b42e14e83712e418b3c367880641369c057
Chloe Chong <chloechong.us@gmail.com>
2018-09-09 10:47:38 -0400

    Finished queue as stack

--- data-structures/queueAsStack.js
+++ data-structures/queueAsStack.js
@@ -1,7 +1,43 @@
+class Node {
+  constructor(val, prev = null) {
+    this.val = val;
+    this.prev = prev;
+    this.next = null;
+  }
+}
+
 class MyQueue {
   constructor() {
     this.top = null;
     this.tail = null;
     this.length = 0;
+  }
+  empty() {
+    return this.length === 0;
+  }
+  push(val) {
+    if (this.top) {
+      const temp = new Node(val, this.tail);
+      this.tail.next = temp;
+      this.tail = temp;
+    } else {
+      this.top = new Node(val);
+      this.tail = this.top;
+    }
+    this.length++;
+  }
+  peek() {
+    return this.top.val;
+  }
+  pop() {
+    const temp = this.top;
+    if (this.top === this.tail) {
+      this.top = null;
+      this.tail = null;
+    } else {
+      this.top = this.top.next;
+    }
+    this.length--;
+    return this.top.val;
   }
 }
