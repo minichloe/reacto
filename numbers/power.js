32a40f4a3adb0351617b2874433338e9d3fc37e5
Chloe Chong <chloechong.us@gmail.com>
2018-09-10 22:28:47 -0400

    Optimised power

--- numbers/power.js
+++ numbers/power.js
@@ -4,7 +4,9 @@ function myPow(x, n) {
     x = 1 / x;
     n = -n;
   }
-  return x * myPow(x, n - 1);
+  if (n % 3 === 0) return myPow(x * x * x, n / 3);
+  else if (n % 2 === 0) return myPow(x * x, n / 2);
+  else return x * myPow(x, n - 1);
 }
 
 const test = myPow(2.1, 3);
