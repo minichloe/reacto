d29c3857909f4d2985add2ae832fc45a0e4d3356
Chloe Chong <chloechong.us@gmail.com>
2018-09-10 22:24:49 -0400

    Added better if case for power

--- numbers/power.js
+++ numbers/power.js
@@ -1,11 +1,11 @@
 function myPow(x, n) {
-  if (n === 1) return x;
+  if (n === 0 || x === 1) return 1;
   if (n < 0) {
     x = 1 / x;
-    n = Math.abs(n);
+    n = -n;
   }
   return x * myPow(x, n - 1);
 }
 
-const test = myPow(2, 10);
+const test = myPow(2.1, 3);
 console.log(test);
