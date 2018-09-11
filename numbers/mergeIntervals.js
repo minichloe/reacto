e73eb15089b05a9da4c434911ec009721fabe0d8
Chloe Chong <chloechong.us@gmail.com>
2018-09-11 18:54:50 -0400

    Fixed merge intervals

--- numbers/mergeIntervals.js
+++ numbers/mergeIntervals.js
@@ -3,14 +3,16 @@ function merge(intervals) {
   let prev = intervals[0];
   for (let i = 1; i < intervals.length; i++) {
     const curr = intervals[i];
-    if (prev[1] < curr[0]) prev = [prev[0], curr[1]];
+    if (prev[1] >= curr[0] && prev[1] < curr[1]) prev = [prev[0], curr[1]];
     else {
       res.push(prev);
       prev = curr;
     }
   }
+  if (res[res.length - 1] !== prev) res.push(prev);
   return res;
 }
 
 const test = merge([[1, 3], [2, 6], [8, 10], [15, 18]]);
-console.log(test);
+const test2 = merge([[1, 4], [4, 5]]);
+console.log(test2);
