87765c0bc933c96971964b945b41ebae984e2937
Chloe Chong <chloechong.us@gmail.com>
2018-09-13 16:23:44 -0400

    Added clearer solution to missing nums

--- numbers/missingNumber.js
+++ numbers/missingNumber.js
@@ -1,11 +1,18 @@
+// function missingNumber(nums) {
+//   let x = 0,
+//     y = nums.length;
+//   for (let i = 0; i < nums.length; i++) {
+//     x += nums[i];
+//     y += i;
+//   }
+//   return y - x;
+// }
+
+// Same solution
 function missingNumber(nums) {
-  let x = 0,
-    y = nums.length;
-  for (let i = 0; i < nums.length; i++) {
-    x += nums[i];
-    y += i;
-  }
-  return y - x;
+  const expectedTotal = (nums.length * (nums.length + 1)) / 2;
+  nums = nums.reduce((a, b) => a + b, 0);
+  return expectedTotal - nums;
 }
 
 const test = missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]);
