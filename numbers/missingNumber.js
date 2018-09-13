928cd8dd2dad40ddd537c79e671eac1079c31669
Chloe Chong <chloechong.us@gmail.com>
2018-09-13 16:15:58 -0400

    O(n) solution for missing number

---
+++ numbers/missingNumber.js
@@ -0,0 +1,12 @@
+function missingNumber(nums) {
+  let x = 0,
+    y = 0;
+  for (let i = 0; i < nums.length; i++) {
+    x += nums[i];
+    y += i;
+  }
+  return y - x;
+}
+
+const test = missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]);
+console.log(test);
