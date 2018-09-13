13407b37746d3c0a9eb59663c692569a1dd9491b
Chloe Chong <chloechong.us@gmail.com>
2018-09-13 14:48:07 -0400

    Duplicate number completed with cyclic LL principles

--- numbers/duplicateNumber.js
+++ numbers/duplicateNumber.js
@@ -1,13 +1,19 @@
 // Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
 
 function findDuplicate(nums) {
-  let slow = 0;
+  if (nums.length <= 2) return nums[0];
+  let slow = nums[0];
   fast = nums[slow];
-  while (nums[slow] != nums[fast]) {
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  }
+  fast = 0;
+  while (slow != fast) {
     slow = nums[slow];
     fast = nums[fast];
   }
-  return nums[slow];
+  return slow;
 }
 
 const test = findDuplicate([3, 1, 3, 4, 2]);
