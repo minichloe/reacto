c5e4afd47728dadb41ea42ffee1900b43abdf5df
Chloe Chong <chloechong.us@gmail.com>
2018-09-13 14:42:30 -0400

    Initialized duplicate number

---
+++ numbers/duplicateNumber.js
@@ -0,0 +1,11 @@
+// Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+function findDuplicate(nums) {
+  let slow = 0;
+  fast = nums[slow];
+  while (nums[slow] != nums[fast]) {
+    slow = nums[fast];
+    fast = nums[slow];
+  }
+  return nums[slow];
+}
