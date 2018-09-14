918673c29c810323d07e9dc8dcba1807176232b6
Chloe Chong <chloechong.us@gmail.com>
2018-09-14 12:46:30 -0400

    Added tests for reverse integer

--- numbers/reverseInteger.js
+++ numbers/reverseInteger.js
@@ -2,10 +2,13 @@
 
 function reverse(x) {
   let res = 0;
-  const n = Math.abs(x);
+  let n = Math.abs(x);
   while (n > 0) {
     res = res * 10 + (n % 10);
     n = Math.floor(n / 10);
   }
   return x > 0 ? res : -res;
 }
+
+const test = reverse(120);
+console.log(test);
