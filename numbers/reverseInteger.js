8c4c8c4ba33ff8cde70769df2ef9b6096a715fb8
Chloe Chong <chloechong.us@gmail.com>
2018-09-14 12:45:50 -0400

    Added reverse integer

---
+++ numbers/reverseInteger.js
@@ -0,0 +1,11 @@
+// Given a 32-bit signed integer, reverse digits of an integer.
+
+function reverse(x) {
+  let res = 0;
+  const n = Math.abs(x);
+  while (n > 0) {
+    res = res * 10 + (n % 10);
+    n = Math.floor(n / 10);
+  }
+  return x > 0 ? res : -res;
+}
