97e42229dc131c272388cfe404f6dde721b32a85
Chloe Chong <chloechong.us@gmail.com>
2018-09-16 11:51:43 -0400

    Kth largest with set

--- further_practice/kthLargestElem.js
+++ further_practice/kthLargestElem.js
@@ -1,3 +1,15 @@
 // Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
-function findKthLargest(nums, k) {}
+function findKthLargest(nums, k) {
+  const set = new Set(nums);
+  let max;
+  while (k) {
+    set.delete(max);
+    max = Math.max(...set);
+    k--;
+  }
+  return max;
+}
+
+const test = findKthLargest([3, 2, 1, 5, 6, 4], 2);
+console.log(test);
