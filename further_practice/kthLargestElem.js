268c71f8d0ed8f8d2c53db42d81b4539c492a7cb
Chloe Chong <chloechong.us@gmail.com>
2018-09-16 11:24:46 -0400

    Initialised leetcode 215

---
+++ further_practice/kthLargestElem.js
@@ -0,0 +1,3 @@
+// Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+function findKthLargest(nums, k) {}
