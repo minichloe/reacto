74086aa558d76c0dc24a0231ecf6b48a60a5bf4a
Chloe Chong <chloechong.us@gmail.com>
2018-09-15 22:51:01 -0400

    Added test cases for reverseWords

--- further_practice/reverseStr.js
+++ further_practice/reverseStr.js
@@ -1,8 +1,11 @@
 function reverseWords(str) {
   str = str.trim().replace(/\s+/gi, ' ');
-  if (str === ' ') return '';
+  // if (str === ' ') return '';
   return str
     .split(' ')
     .reverse()
     .join(' ');
 }
+
+const test = reverseWords('   ');
+console.log('a' + test + 'a');
