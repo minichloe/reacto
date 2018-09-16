b1e4664bce86506517637345cb6e30e3b7ee69be
Chloe Chong <chloechong.us@gmail.com>
2018-09-15 22:28:29 -0400

    Simplified isValidBST

--- data-structures/validateBST.js
+++ data-structures/validateBST.js
@@ -1,11 +1,17 @@
-function isValidBST(root) {
-  if (root.left) {
-    if (root.left.val > root.val) return false;
-    else root.left = isValidBST(root.left);
-  }
-  if (root.right) {
-    if (root.right.val < root.val) return false;
-    else root.right = isValidBST(root.right);
-  }
-  return true;
+// function isValidBST(root) {
+//   if (!root) return true;
+//   return (function check(root, min = -Infinity, max = Infinity) {
+//     if (!root) return true;
+//     if (root.val >= max || root.val <= min) return false;
+//     return check(root.left, min, root.val) && check(root.right, root.val, max);
+//   })(root);
+// }
+
+function isValidBST(root, min = -Infinity, max = Infinity) {
+  if (!root) return true;
+  if (root.val >= max || root.val <= min) return false;
+  return (
+    isValidBST(root.left, min, root.val) &&
+    isValidBST(root.right, root.val, max)
+  );
 }
