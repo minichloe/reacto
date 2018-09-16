cc3db9326e26f1f62b4bc0764a0eb76c96973d60
Chloe Chong <chloechong.us@gmail.com>
2018-09-16 11:54:29 -0400

    kth largest with built in sorting

--- further_practice/kthLargestElem.js
+++ further_practice/kthLargestElem.js
@@ -1,14 +1,16 @@
 // Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
 function findKthLargest(nums, k) {
-  const set = new Set(nums);
-  let max;
-  while (k) {
-    set.delete(max);
-    max = Math.max(...set);
-    k--;
-  }
-  return max;
+  nums = nums.sort((a, b) => a - b);
+  return nums[nums.length - k];
+  // const set = new Set(nums);
+  // let max;
+  // while (k) {
+  //   set.delete(max);
+  //   max = Math.max(...set);
+  //   k--;
+  // }
+  // return max;
 }
 
 const test = findKthLargest([3, 2, 1, 5, 6, 4], 2);
