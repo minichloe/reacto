5702d676947c2054886391b0b1aaebbe7f12fd82
Chloe Chong <chloechong.us@gmail.com>
2018-09-17 23:27:35 -0400

    Added explanation in merge sorted array

--- further_practice/mergeArr.js
+++ further_practice/mergeArr.js
@@ -3,6 +3,7 @@ function merge(nums1, m, nums2, n) {
   m--;
   n--;
   while (m + n >= -1) {
+    //If bigger, or nothing left in nums2, copy it over
     if (nums1[m] > nums2[n] || n < 0) {
       nums1[m + n + 1] = nums1[m];
       m--;
