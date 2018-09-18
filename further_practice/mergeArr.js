798bb4786c400b4fde3560b63bb890c2023e4742
Chloe Chong <chloechong.us@gmail.com>
2018-09-17 23:26:06 -0400

    merge sorted array using two pointers from back

---
+++ further_practice/mergeArr.js
@@ -0,0 +1,14 @@
+// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+function merge(nums1, m, nums2, n) {
+  m--;
+  n--;
+  while (m + n >= -1) {
+    if (nums1[m] > nums2[n] || n < 0) {
+      nums1[m + n + 1] = nums1[m];
+      m--;
+    } else {
+      nums1[m + n + 1] = nums2[n];
+      n--;
+    }
+  }
+}
