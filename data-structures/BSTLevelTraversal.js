b442dc2440eb86fef28a40bb8883004ebcd583b3
Chloe Chong <chloechong.us@gmail.com>
2018-09-18 11:12:16 -0400

    Finished bst level order using queues

--- data-structures/BSTLevelTraversal.js
+++ data-structures/BSTLevelTraversal.js
@@ -1,8 +1,19 @@
 function levelOrder(root) {
   if (!root) return [];
-  const res = [[root.value]];
+  const res = [[root.val]];
   const queue = [root];
   while (queue.length) {
     const temp = queue.shift();
+    const tempVal = [];
+    if (temp.left) {
+      queue.push(temp.left);
+      tempVal.push(temp.left.val);
+    }
+    if (temp.right) {
+      queue.push(temp.right);
+      tempVal.push(temp.right.val);
+    }
+    if (tempVal.length) res.push(tempVal);
   }
+  return res;
 }
