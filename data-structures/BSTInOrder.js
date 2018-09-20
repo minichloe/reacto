1925004ce5afc20110c99d30e401a8788c6ee6a2
Chloe Chong <chloechong.us@gmail.com>
2018-09-20 16:16:39 -0400

    BST in order complete

--- data-structures/BSTInOrder.js
+++ data-structures/BSTInOrder.js
@@ -1,5 +1,6 @@
 // Recursive solution
-function inorderTraversal(root, res = []) {
+function inorderTraversal(root) {
+  if (!root) return [];
   return [
     ...inorderTraversal(root.left),
     root.val,
@@ -8,3 +9,19 @@ function inorderTraversal(root, res = []) {
 }
 
 // Iterative solution
+function inorderTraversal(root) {
+  const res = [];
+  if (!root) return res;
+  const stack = [root];
+  let currNode = root.left;
+  while (currNode || stack.length) {
+    while (currNode) {
+      stack.push(currNode);
+      currNode = currNode.left;
+    }
+    currNode = stack.pop();
+    res.push(currNode.val);
+    currNode = currNode.right;
+  }
+  return res;
+}
