372626e759979f23dc3d473a4fd138a691b835d7
Chloe Chong <chloechong.us@gmail.com>
2018-09-20 16:54:36 -0400

    post order traversal complered

--- data-structures/BSTpostorder.js
+++ data-structures/BSTpostorder.js
@@ -1,8 +1,23 @@
+// Recursive solution
+// function postorderTraversal(root) {
+//   if (!root) return [];
+//   return [
+//     ...postorderTraversal(root.left),
+//     ...postorderTraversal(root.right),
+//     root.val,
+//   ];
+// }
+
+// Iterative solution
 function postorderTraversal(root) {
   if (!root) return [];
-  return [
-    ...postorderTraversal(root.left),
-    ...postorderTraversal(root.right),
-    root.val,
-  ];
+  const res = [];
+  const stack = [root];
+  while (stack.length) {
+    const node = stack.pop();
+    res.unshift(node.val);
+    if (node.left) stack.push(node.left);
+    if (node.right) stack.push(node.right);
+  }
+  return res;
 }
