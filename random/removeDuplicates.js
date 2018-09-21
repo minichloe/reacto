1650fb9308965ada86b1a0565cdb5eede3c48ee6
Chloe Chong <chloechong.us@gmail.com>
2018-09-21 10:56:53 -0400

    Refactored remove duplicates

--- random/removeDuplicates.js
+++ random/removeDuplicates.js
@@ -1,16 +1,15 @@
 function removeDuplicates(nums) {
   if (!nums.length) return 0;
-  let curr = nums[0];
-  let length = 1;
-  for (let i = 1, j = 1; i < nums.length; i++) {
+  let curr = nums[0],
+    j = 1;
+  for (let i = 1; i < nums.length; i++) {
     if (nums[i] !== curr) {
       curr = nums[i];
       nums[j] = curr;
-      length++;
       j++;
     }
   }
-  return length;
+  return j;
 }
 
 const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
