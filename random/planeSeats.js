5e1c4ec1ab952e75d0b94f06619ab21f4301e81d
Chloe Chong <chloechong.us@gmail.com>
2018-09-21 12:38:53 -0400

    Added comments to plane seats

--- random/planeSeats.js
+++ random/planeSeats.js
@@ -1,24 +1,29 @@
 // Given N row of seats with 10 seats each in a 3-4-3 configuration (A - K with I omitted), return the max number of 3 seat pairings (aisle between seats is not a valid pairing)
 
-function solution(rows, S) {
-  if (!S.length) return rows * 3;
-  S = new Set(S.toUpperCase().split(' '));
+function planeSeats(rows, S) {
+  // If there are no reserved seats, each row can seat three families
+  // if (!S.length) return rows * 3;
+  // Create array of reserved seats
+  S = S.toUpperCase().split(' ');
+  // Convert array to set for constant time lookup
+  S = new Set(S);
   let results = 0;
-  console.log(S);
   for (let i = 1; i <= rows; i++) {
+    // Check 'abc' row is empty
     if (!S.has(`${i}A`) && !S.has(`${i}B`) && !S.has(`${i}C`)) {
       results++;
     }
+    // Check 'hjk' row is empty
     if (!S.has(`${i}H`) && !S.has(`${i}J`) && !S.has(`${i}K`)) {
       results++;
     }
+    // Check middle seats are not taken
     if (!S.has(`${i}E`) && !S.has(`${i}F`)) {
       if (!S.has(`${i}D`) || S.has(`${i}G`)) results++;
     }
-    console.log(i, results);
   }
   return results;
 }
 
-const test = solution(10, '1A 2B 10D');
+const test = planeSeats(10, '');
 console.log(test);
