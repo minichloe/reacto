f971e324e0d2a9a76809754d5420699a9e728b07
Chloe Chong <chloechong.us@gmail.com>
2018-09-25 22:10:38 -0400

    Finished find start of LL cycle

--- data-structures/detectCycleLL.js
+++ data-structures/detectCycleLL.js
@@ -1,1 +1,17 @@
 // Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+function detectCycle(head) {
+  if (!head || !head.next || !head.next.next) return null;
+  let slow = head.next;
+  let fast = head.next.next;
+  while (slow !== fast) {
+    slow = slow.next;
+    if (!fast.next.next) return null;
+    fast = fast.next.next;
+  }
+  let fast = head;
+  while (slow !== fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return fast;
+}
