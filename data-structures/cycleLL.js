c98c68c2731d1e7f2746fc9d36e840d81a74db47
Chloe Chong <chloechong.us@gmail.com>
2018-09-25 21:58:01 -0400

    Initiated linked list cycle

--- data-structures/cycleLL.js
+++ data-structures/cycleLL.js
@@ -1,15 +1,4 @@
 // O(n) time, O(1) space
-function hasCycle(head) {
-  if (!head || !head.next) return false;
-  let slowNode = head;
-  let fastNode = head.next;
-  while (slowNode !== fastNode) {
-    if (!fastNode || !fastNode.next) return false;
-    slowNode = slowNode.next;
-    fastNode = fastNode.next.next;
-  }
-  return true;
-}
 
 // O(n) time and space
 function hasCycle(head) {
