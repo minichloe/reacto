a16c6ee04141dd8793ad6bf368f30a9452d61c4c
Chloe Chong <chloechong.us@gmail.com>
2018-09-25 21:59:22 -0400

    Completed linked list cycle

--- data-structures/cycleLL.js
+++ data-structures/cycleLL.js
@@ -1,4 +1,16 @@
 // O(n) time, O(1) space
+function hasCycle(head) {
+  let node = head;
+  if (!node || !node.next) return false;
+  let fastNode = node.next;
+  while (fastNode) {
+    node = node.next;
+    if (!fastNode.next) return false;
+    fastNode = fastNode.next.next;
+    if (node === fastNode) return true;
+  }
+  return false;
+}
 
 // O(n) time and space
 function hasCycle(head) {
