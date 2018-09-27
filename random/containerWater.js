a576766206d5b82697cdde8ad078087ff508a8b9
Chloe Chong <chloechong.us@gmail.com>
2018-09-27 12:20:57 -0400

    Container water completed with tests

--- random/containerWater.js
+++ random/containerWater.js
@@ -3,9 +3,12 @@ const maxArea = height => {
   let left = 0,
     right = height.length - 1;
   while (left < right) {
-    const currBox = (right - left) * Math.min(height[left], height[j]);
+    const currBox = (right - left) * Math.min(height[left], height[right]);
     max = Math.max(max, currBox);
     height[left] <= height[right] ? left++ : right--;
   }
   return max;
 };
+
+const test = maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
+console.log(test); // 49
