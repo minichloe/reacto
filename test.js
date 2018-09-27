185b2fd97470016d820a78d86e9143b8c1b7ad6b
Chloe Chong <chloechong.us@gmail.com>
2018-09-27 12:15:17 -0400

    Practicing tests

--- test.js
+++ test.js
@@ -1,5 +1,45 @@
-function longestPalindrome(str) {
-  if (str.length <= 1) return str;
-  let start = 0,
-    end = 0;
+function threeSum(arr) {
+  arr = arr.sort((a, b) => a - b);
+  if (arr[0] > 0) return [];
+  const res = [];
+  for (let i = 0; i < arr.length - 2; i++) {
+    if (arr[i] > 0) return res;
+    if (i > 0 && arr[i] == arr[i - 1]) continue;
+    let start = i + 1;
+    let end = arr.length - 1;
+    while (start < end) {
+      const sum = arr[i] + arr[start] + arr[end];
+      if (sum === 0) {
+        res.push([arr[i], arr[start], arr[end]]);
+        start++;
+        end--;
+        while (start < end && arr[start] === arr[start - 1]) start++;
+        while (start < end && arr[end] === arr[end + 1]) end--;
+      } else {
+        sum > 0 ? end-- : start++;
+      }
+    }
+  }
+  return res;
+}
+
+function threeSumClosest(arr, target) {
+  let res = Infinity;
+  arr = arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length - 2; i++) {
+    let start = i + 1;
+    let end = arr.length - 1;
+    while (start < end) {
+      const sum = arr[i] + arr[start] + arr[end];
+      if (Math.abs(target - sum) < Math.abs(target - res)) res = sum;
+      if (sum < target) {
+        start++;
+        while (arr[start] === arr[start - 1]) start++;
+      } else {
+        end--;
+        while (arr[end] === arr[end + 1]) end--;
+      }
+    }
+  }
+  return res;
 }
