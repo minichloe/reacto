18ad440f1f2e1e1c1b823eb7264d465822644744
Chloe Chong <chloechong.us@gmail.com>
2018-09-26 22:52:01 -0400

    zig zag complete with test cases

--- random/zigzagConversion.js
+++ random/zigzagConversion.js
@@ -3,9 +3,13 @@ function convert(str, numRows) {
   let idx = 0,
     direction = 1;
   for (let i = 0; i < str.length; i++) {
+    console.log(idx, direction);
     res[idx] += str[i];
     idx += direction;
-    if (!idx || idx >= numRows) direction = -1;
+    if (!idx || idx >= numRows - 1) direction *= -1;
   }
   return res.join('');
 }
+
+const test = convert('PAYPALISHIRING', 4);
+console.log(test);
