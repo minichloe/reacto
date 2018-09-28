5b404f2aadfb1ed7616dd539c00cfc49992cdc8b
Chloe Chong <chloechong.us@gmail.com>
2018-09-28 15:47:49 -0400

    Unique paths 2 completed

--- further_practice/uniquePaths.js
+++ further_practice/uniquePaths.js
@@ -15,21 +15,19 @@ function uniquePaths(m, n) {
 
 function uniquePathsWithObstacles(grid) {
   // Initialize board with length of a single row and fill with 1
-  const board = new Array(grid[0].length).fill(1);
-
+  const board = new Array(grid[0].length).fill(0);
+  // Set starting point to 1 (one path to get there)
+  board[0] = 1;
   // Loop through each row of the grid
   for (let i = 0; i < grid.length; i++) {
-    for (let j = 1; j < board.length; j++) {
-      // Do nothing in the first row as there is only one unique path
-      if (i === 0 && grid[i][j] !== 1) continue;
-
+    for (let j = 0; j < board.length; j++) {
       // If there is an obstacle that path is impossible and set to 0
       if (grid[i][j] === 1) board[j] = 0;
-      else board[j] += board[j - 1];
+      else if (j > 0) board[j] += board[j - 1];
     }
   }
   return board[board.length - 1];
 }
 
-const test2 = uniquePathsWithObstacles([[0, 0, 0], [0, 1, 0], [0, 0, 0]]);
+const test2 = uniquePathsWithObstacles([[0], [1]]);
 console.log(test2); // 2
