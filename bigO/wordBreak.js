1769b99b2c3addd59c573d02b1eb1e01e3302b49
Chloe Chong <chloechong.us@gmail.com>
2018-10-02 12:49:42 -0400

    Used trie in word break'

--- bigO/wordBreak.js
+++ bigO/wordBreak.js
@@ -8,8 +8,29 @@ function wordBreak(s, wordDict) {
       node = node[char];
     }
   });
-  return trie;
+  let start = 0,
+    end = 0;
+  let node = trie;
+  const res = [];
+  for (let i = 0; i < s.length; i++) {
+    if (start === end && !node.hasOwnProperty(s[i])) return false;
+    if (!node.hasOwnProperty(s[i])) {
+      res.push(s.substring(start, end));
+      start = i;
+      end = start;
+      node = trie[s[i]];
+      if (!node) return false;
+    } else {
+      end++;
+      node = node[s[i]];
+    }
+    if (i === s.length - 1) {
+      res.push(s.substring(start));
+    }
+  }
+  console.log(res);
+  return true;
 }
 
-const test = wordBreak('leetcode', ['leet', 'code']);
+const test = wordBreak('applepenapple', ['apple', 'pen']);
 console.log(test);
