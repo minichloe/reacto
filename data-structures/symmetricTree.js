1cc63c69abc71d60883a4a3ac7e6af1ec23eee1b
Chloe Chong <chloechong.us@gmail.com>
2018-10-03 16:21:23 -0400

    Added palindrome check to symmetric tree

--- data-structures/symmetricTree.js
+++ data-structures/symmetricTree.js
@@ -8,3 +8,23 @@ function isSymmetric(root) {
   }
   return mirror(root.left, root.right);
 }
+
+// Iterative solution
+function isSymmetricIterative(root) {
+  if (!root) return true;
+  if ((!root.left && root.right) || (!root.right && root.left)) return false;
+  const queue = [root];
+  while (queue.length) {
+    const currNode = queue.shift();
+  }
+}
+
+function checkPalindrome(list) {
+  for (let i = 0, j = list.length - 1; i < j; i++, j--) {
+    if (list[i] !== list[j]) return false;
+  }
+  return true;
+}
+
+const test = checkPalindrome([1, 2, 3, 2, 1]);
+console.log(test);
