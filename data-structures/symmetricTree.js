4952218b7d7e06986da9e8965d810bd706dfc277
Chloe Chong <chloechong.us@gmail.com>
2018-10-03 16:16:21 -0400

    Recursive solution for symmetric tree

--- data-structures/symmetricTree.js
+++ data-structures/symmetricTree.js
@@ -1,1 +1,10 @@
-function isSymmetric(root) {}
+// Recursive solution
+function isSymmetric(root) {
+  if (!root) return true;
+  function mirror(a, b) {
+    if (!a && !b) return true;
+    if (!a || !b || a.val !== b.val) return false;
+    return mirror(a.left, b.right) && mirror(a.right, b.left);
+  }
+  return mirror(root.left, root.right);
+}
