2aa12d17fa5999f6284ffe337d519c56a05787f4
Chloe Chong <chloechong.us@gmail.com>
2018-10-10 19:25:18 -0400

    Jump game completed using tracking backwards

--- numbers/jumpGame.js
+++ numbers/jumpGame.js
@@ -2,15 +2,27 @@
 // Each element in the array represents your maximum jump length at that position.
 // Determine if you are able to reach the last index.
 
-function canJump(nums, end, idx = 0) {
+// Max stack
+function canJumpRecursive(nums, end, idx = 0, visited = {}) {
+  if (nums.length === 1) return true;
+  visited[idx] = true;
   end = end || nums.length - 1;
   const curr = new Array(nums[idx]).fill(1).map((x, i) => x + i);
   return curr.some(x => {
     if (idx + x === end) return true;
     else if (x === 0) return false;
-    else return canJump(nums, end, idx + x);
+    else if (!visited[idx + x]) return canJump(nums, end, idx + x);
+    else return false;
   });
 }
 
-const test = canJump([3, 2, 1, 0, 4]);
+function canJump(nums) {
+  let end = nums.length - 1;
+  for (let i = end - 1; i >= 0; i--) {
+    if (nums[i] >= end - i) end = i;
+  }
+  return end === 0;
+}
+
+const test = canJump([3, 2, 1, 1, 4]);
 console.log(test);
