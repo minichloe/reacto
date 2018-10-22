5ef99395237aa100e32b70c8a5e353abc0cbf772
Chloe Chong <chloechong.us@gmail.com>
2018-10-22 18:03:25 -0400

    Added iterative solution to remove nodes

--- data-structures/removeNodes.js
+++ data-structures/removeNodes.js
@@ -4,3 +4,24 @@ function removeNodes(listHead, x) {
   if (listHead.data > x) return listHead.next;
   else return listHead;
 }
+
+const removeElements = function(head, val) {
+  while (head && head.val === val) head = head.next;
+  if (!head) return null;
+  if (!head.next) return head;
+  let prev = head;
+  let curr = head.next;
+  while (curr) {
+    if (curr.val === val) {
+      while (curr && curr.val === val) curr = curr.next;
+      prev.next = curr;
+      prev = prev.next;
+      if (prev) curr = prev.next;
+      else break;
+    } else {
+      prev = curr;
+      curr = curr.next;
+    }
+  }
+  return head;
+};
