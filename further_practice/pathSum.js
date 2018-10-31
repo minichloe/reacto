d89cd4ac7ddb1a248c5be674d8b90a50b624dd39
Chloe Chong <chloechong.us@gmail.com>
2018-10-30 22:49:29 -0400

    Path sum completed

--- further_practice/pathSum.js
+++ further_practice/pathSum.js
@@ -1,5 +1,5 @@
 function hasPathSum(root, sum) {
-  if (!root || root.val > sum) return false;
-  if (root.val === sum) return true;
+  if (!root) return false;
+  if (root.val === sum && !root.left && !root.right) return true;
   return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
 }
