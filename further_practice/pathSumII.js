46553fc37c437b95930c1f799436df0e14212917
Chloe Chong <chloechong.us@gmail.com>
2018-11-04 22:54:45 -0500

    Fixed pathsumII

--- further_practice/pathSumII.js
+++ further_practice/pathSumII.js
@@ -1,10 +1,10 @@
 function pathSum(root, sum) {
   const results = [];
-  allPaths();
+  allPaths(root, sum);
   function allPaths(root, sum, nums = []) {
-    if (!root || root.val > sum) return;
+    if (!root) return;
     if (root.val === sum && !root.left && !root.right) {
-      results.push([...num, root.val]);
+      results.push([...nums, root.val]);
       return;
     }
     return allPaths(root.left, sum - root.val, [...nums, root.val]) || allPaths(root.right, sum - root.val, [...nums, root.val]);
