8ae11a8dde6e9b6b15a675b4ae9aabd419831bc4
Chloe Chong <chloechong.us@gmail.com>
2018-11-04 23:02:23 -0500

    Completed path sum II

--- further_practice/pathSumII.js
+++ further_practice/pathSumII.js
@@ -3,11 +3,11 @@ function pathSum(root, sum) {
   allPaths(root, sum);
   function allPaths(root, sum, nums = []) {
     if (!root) return;
-    if (root.val === sum && !root.left && !root.right) {
-      results.push([...nums, root.val]);
-      return;
-    }
-    return allPaths(root.left, sum - root.val, [...nums, root.val]) || allPaths(root.right, sum - root.val, [...nums, root.val]);
+    nums.push(root.val);
+    if (root.val === sum && !root.left && !root.right) results.push([...nums]);
+    allPaths(root.left, sum - root.val, nums);
+    allPaths(root.right, sum - root.val, nums);
+    nums.pop();
   }
   return results;
 }
