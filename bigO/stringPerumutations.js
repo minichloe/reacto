d8641cfc88b8de682a9b41fc445aefaf7a50c695
Chloe Chong <chloechong.us@gmail.com>
2018-11-05 21:47:10 -0500

    Added more string permutation

--- bigO/stringPerumutations.js
+++ bigO/stringPerumutations.js
@@ -1,26 +1,44 @@
+// function stringPerumutations(str) {
+//   if (str.length === 1) return [str];
+//   const all = [];
+//   let i = 0;
+//   while (i < str.length) {
+//     const letter = str[i];
+//     const otherChars = str.slice(0, i) + str.slice(i + 1);
+//     stringPerumutations(otherChars).forEach(subPerm => {
+//       all.push(letter + subPerm);
+//     });
+//     while (str[i] === letter) i++;
+//     console.log(all);
+//   }
+//   return all;
+// }
+
+// function sortStringPermutations(str) {
+//   str = str
+//     .split('')
+//     .sort()
+//     .join('');
+//   const results = stringPerumutations(str);
+//   console.log(results);
+// }
+
+// sortStringPermutations('app');
+
 function stringPerumutations(str) {
-  if (str.length === 1) return [str];
-  const all = [];
+  if (str.length <= 1) return [str];
+  const res = [];
   let i = 0;
   while (i < str.length) {
-    const letter = str[i];
-    const otherChars = str.slice(0, i) + str.slice(i + 1);
-    stringPerumutations(otherChars).forEach(subPerm => {
-      all.push(letter + subPerm);
+    const char = str[i];
+    const otherChars = str.slice(0, i) + str.slice(i);
+    stringPerumutations(otherChars).forEach(x => {
+      res.push(char + x);
     });
-    while (str[i] === letter) i++;
-    console.log(all);
+    while (str[i] === char) i++;
   }
-  return all;
+  return res;
 }
 
-function sortStringPermutations(str) {
-  str = str
-    .split('')
-    .sort()
-    .join('');
-  const results = stringPerumutations(str);
-  console.log(results);
-}
-
-sortStringPermutations('app');
+const test = stringPerumutations('app');
+console.log(test);
--- further_practice/palindromePartitioning.js
+++ further_practice/palindromePartitioning.js
@@ -1,23 +1,56 @@
 // Given a string s, partition s such that every substring of the partition is a palindrome.
 
-function partition(s) {
+// function partition(s) {
+//   const res = [];
+//   const curr = [];
+//   if (s === null || !s.length) return res;
+//   addPalindrome(s, 0, curr);
+//   return res;
+
+//   function addPalindrome(s, start, curr) {
+//     if (start === s.length) {
+//       res.push([...curr]);
+//       return;
+//     }
+//     for (let i = start + 1; i <= s.length; i++) {
+//       const str = s.slice(start, i);
+//       if (isPalindrome(str)) {
+//         curr.push(str);
+//         addPalindrome(s, i, curr);
+//         // curr.splice(curr.length - 1, 1);
+//         curr.pop();
+//       }
+//     }
+//   }
+
+//   function isPalindrome(str) {
+//     let left = 0,
+//       right = str.length - 1;
+//     while (left < right) {
+//       if (str[left] !== str[right]) return false;
+//       left++;
+//       right--;
+//     }
+//     return true;
+//   }
+// }
+
+function partition(str) {
+  if (str === null || !str.length) return [];
   const res = [];
-  const curr = [];
-  if (s === null || !s.length) return res;
-  addPalindrome(s, 0, curr);
+  addPalindrome(str, 0, []);
   return res;
 
-  function addPalindrome(s, start, curr) {
-    if (start === s.length) {
-      res.push([...curr]);
+  function addPalindrome(str, start, curr) {
+    if (start === str.length) {
+      res.push(curr.slice());
       return;
     }
-    for (let i = start + 1; i <= s.length; i++) {
-      const str = s.slice(start, i);
-      if (isPalindrome(str)) {
-        curr.push(str);
-        addPalindrome(s, i, curr);
-        // curr.splice(curr.length - 1, 1);
+    for (let i = start + 1; i <= str.length; i++) {
+      const subStr = str.slice(start, i);
+      if (isPalindrome(subStr)) {
+        curr.push(subStr);
+        addPalindrome(str, i, curr);
         curr.pop();
       }
     }
