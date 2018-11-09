7621608108e815e389ee9f4e2c10975090ab391c
Chloe Chong <chloechong.us@gmail.com>
2018-11-09 13:14:09 -0500

    Search in rotated array completed with tests

--- further_practice/searchRotatedArr.js
+++ further_practice/searchRotatedArr.js
@@ -1,4 +1,19 @@
 function search(nums, target) {
+  if (nums[0] === target) return 0;
+  let left = 0,
+    right = nums.length - 1;
+  let pivot = findPivot(nums);
+  if (nums[pivot] === target) return pivot;
+  else if (pivot > 0) target > nums[right] ? (right = pivot - 1) : (left = pivot + 1);
+  else left++;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    nums[mid] < target ? (left = mid + 1) : (right = mid - 1);
+  }
+  return -1;
+
   function findPivot(nums) {
     let left = 0,
       right = nums.length - 1;
@@ -9,18 +24,7 @@ function search(nums, target) {
       nums[left] >= nums[mid] ? (right = mid - 1) : (left = mid + 1);
     }
   }
-
-  if (nums.length === 1 && nums[0] === target) return 0;
-  let left = 0,
-    right = nums.length - 1;
-  let pivot = findPivot(nums);
-  if (nums[pivot] === target) return pivot;
-  else if (pivot > 0) target > nums[right] ? (right = pivot - 1) : (left = pivot + 1);
-
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    nums[mid] < target ? (left = mid + 1) : (right = mid - 1);
-  }
-  return -1;
 }
+
+const test = search([3, 1], 1);
+console.log(test);
