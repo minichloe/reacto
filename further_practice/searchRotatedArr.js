2a4eed3e095d1dedb366625b8d6d992c9519bcad
Chloe Chong <chloechong.us@gmail.com>
2018-11-09 13:08:16 -0500

    Added find pivot function

--- further_practice/searchRotatedArr.js
+++ further_practice/searchRotatedArr.js
@@ -1,1 +1,26 @@
-function search(nums, target) {}
+function search(nums, target) {
+  function findPivot(nums) {
+    let left = 0,
+      right = nums.length - 1;
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      if (mid < right && nums[mid] > nums[mid + 1]) return mid;
+      if (mid > left && nums[mid] < nums[mid - 1]) return mid - 1;
+      nums[left] >= nums[mid] ? (right = mid - 1) : (left = mid + 1);
+    }
+  }
+
+  if (nums.length === 1 && nums[0] === target) return 0;
+  let left = 0,
+    right = nums.length - 1;
+  let pivot = findPivot(nums);
+  if (nums[pivot] === target) return pivot;
+  else if (pivot > 0) target > nums[right] ? (right = pivot - 1) : (left = pivot + 1);
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    nums[mid] < target ? (left = mid + 1) : (right = mid - 1);
+  }
+  return -1;
+}
